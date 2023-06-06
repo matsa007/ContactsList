674d25a43551562bfb3991ff63649f48d4b350d2
matsa_007 <serg.arbitran@gmail.com>
2023-06-06 18:36:13 +0300

    storyboard deleted

--- ContactsList/ContactsListViewController.swift
+++ ContactsList/ContactsListViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContactsListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .green
     }
-
 
 }
 
